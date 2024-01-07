95656a2f1ed4c90db34a5eff63f684681c4a1bbc
Jaspev <jacksonpeck12@gmail.com>
2024-01-07 13:13:33 -0700

    enemies collide with eachother now, fullscreen/exit keybinds exist now

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	pass 
+	
 func _process(delta):
 	pass
+
+func _input(event):
+	if event.is_action_pressed("fullscreen"): # f11 fullscreens
+		if DisplayServer.window_get_mode() == 0:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	
+	if event.is_action_pressed("closegame"): # esc closes game
+		get_tree().quit()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,19 @@
 extends Area2D
+@onready var viewport_size = get_viewport_rect().size
 
 func _ready():
 	pass
 	
 func _process(delta):
-	position.x = get_viewport().get_mouse_position().x
-	position.y = get_viewport().get_mouse_position().y
+	var cursor_pos = get_viewport().get_mouse_position()
+	position = cursor_pos
+	
+	# player position cannot be more than viewport bounds
+	if position.x >= viewport_size.x:
+		position.x = viewport_size.x
+	if position.x <= 0:
+		position.x = 0
+	if position.y >= viewport_size.y:
+		position.y = viewport_size.y
+	if position.y <= 0:
+		position.y = 0
