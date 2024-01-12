ef1dba3d518a39097aa6a4686e9663e9e304d773
Jaspev <jacksonpeck12@gmail.com>
2024-01-12 16:06:24 -0700

    enemy finally spawns outside of screen

--- scripts/main.gd
+++ scripts/main.gd
@@ -12,14 +12,42 @@ func _ready():
 	
 func _on_timer_timeout():
 	var instanced_enemy01 = enemy01.instantiate()
+	var coin
+	var pos_x
+	var pos_y
 	
-	randomize()
-	var pos_x = rng.randf_range(-50,viewport_size.x+50)
-	randomize()
-	var pos_y = rng.randf_range(-50,viewport_size.y+50)
+	randomize() #LIFE IS PAIN i don't think i need to call randomize this much but there's no harm it it... probably
+	coin = rng.randf_range(-1,1)
+	if coin >= 0: #IF POSITIVE, SPAWN ENEMY ON EITHER LEFT OF RIGHT
+		randomize()
+		coin = rng.randf_range(-1,1)
+		if coin >= 0: #IF POSITIVE, SPAWN ENEMY ON LEFT
+			randomize()
+			pos_x = rng.randf_range(-50,-10)
+			randomize()
+			pos_y = rng.randf_range(-50,viewport_size.y+50)
+		else: #IF NEGATIVE, SPAWN ENEMY ON RIGHT
+			randomize()
+			pos_x = rng.randf_range(viewport_size.x+10,viewport_size.x+50)
+			randomize()
+			pos_y = rng.randf_range(-50,viewport_size.y+50)
+	else: #IF NEGATIVE, SPAWN ENEMY ON EITHER TOP OR BOTTOM
+		randomize()
+		coin = rng.randf_range(-1,1)
+		if coin >= 0: #IF POSITIVE, SPAWN ENEMY ON TOP
+			randomize()
+			pos_x = rng.randf_range(-50,viewport_size.x+50)
+			randomize()
+			pos_y = rng.randf_range(-50,-10)
+		else: #IF NEGATIVE, SPAWN ENEMY ON BOTTOM
+			randomize()
+			pos_x = rng.randf_range(-50,viewport_size.x+50)
+			randomize()
+			pos_y = rng.randf_range(viewport_size.y+10,viewport_size.y+50)
+	
 	var rand_pos = Vector2(pos_x,pos_y)
 	
 	instanced_enemy01.position = rand_pos
 	
-	print("enemy01 spawned")
+	print("enemy01 spawned @ ", rand_pos)
 	add_child(instanced_enemy01)
--- scripts/player2.gd
+++ scripts/player2.gd
@@ -14,16 +14,3 @@ func _process(delta):
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		var dir = global_position.direction_to(get_global_mouse_position())
 		apply_impulse(dir * hit_force)
-
-	if position.x >= viewport_size.x:
-		position.x = viewport_size.x
-	if position.x <= 0:
-		position.x = 1
-	if position.x >= 320:
-		position.x = 319
-	if position.y >= viewport_size.y:
-		position.y = viewport_size.y
-	if position.y <= 0:
-		position.y = 1
-	if position.y >= 180:
-		position.y = 179
