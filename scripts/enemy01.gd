7f8f23bc73e8821414cfdb884bf86652c3eff4e7
Jaspev <jacksonpeck12@gmail.com>
2024-01-12 03:01:16 -0700

    enemies spawn randomly on screen, not spawning offscreen. pls don't break.

--- scripts/enemy01.gd
+++ scripts/enemy01.gd
@@ -1,13 +1,9 @@
 extends CharacterBody2D
 @onready var player = $"../Player"
 var speed = 50
-
-func _ready():
-	pass 
 	
 func _physics_process(delta):
-	#constantly move toward the player
-	position = position.move_toward(player.position, delta * speed)
+	position = position.move_toward(player.position, delta * speed) #constantly move toward the player
 	
 	#somehow makes them collide with eachother without everything breaking
 	var collision_info = move_and_collide(velocity * delta)
---
+++ scripts/main.gd
@@ -0,0 +1,25 @@
+extends Node2D
+@onready var timer = Timer.new()
+@onready var rng = RandomNumberGenerator.new()
+@onready var enemy01 = preload("res://scenes/enemy01.tscn")
+@onready var viewport_size = get_viewport_rect().size
+
+func _ready():
+	timer.wait_time = 1.0
+	timer.autostart = true
+	timer.timeout.connect(_on_timer_timeout)
+	add_child(timer)
+	
+func _on_timer_timeout():
+	var instanced_enemy01 = enemy01.instantiate()
+	
+	randomize()
+	var pos_x = rng.randf_range(-50,viewport_size.x+50)
+	randomize()
+	var pos_y = rng.randf_range(-50,viewport_size.y+50)
+	var rand_pos = Vector2(pos_x,pos_y)
+	
+	instanced_enemy01.position = rand_pos
+	
+	print("enemy01 spawned")
+	add_child(instanced_enemy01)
